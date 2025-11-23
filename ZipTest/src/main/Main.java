ab6ffd6593843c56f58b4d4635c6e113105ee530
IosebiGames <127665740+IosebiGames@users.noreply.github.com>
2025-11-23 12:48:45 +0400

    Delete ZipTest/src directory

--- ZipTest/src/main/Main.java
+++
@@ -1,9 +0,0 @@
-package main;
-
-public class Main {
-	public static void main(String[] args) {
-		ZipManager zm = new ZipManager("ZipFile.zip");
-	    zm.add("C:\\Users\\IosebiGames\\eclipse-workspace\\ZipTest\\src\\content\\myFile.txt");
-        zm.setPassword(new char[] {'A', 'B','C', '1', '2', '3'});
-   }
-}
