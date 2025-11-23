33991f1cf7781f9d863f50182414288ca18b299d
IosebiGames <127665740+IosebiGames@users.noreply.github.com>
2025-11-23 12:49:50 +0400

    Main.java

--- ZipTest/src/main/Main.java
+++ ZipTest/src/main/Main.java
@@ -7,10 +7,11 @@ public class Main {
 		zm.extractZip("content.txt", "/src/destination");
 		
 		// Parameters:
-		// createZip(content file)
+		// add(content file)
 		// extractZip(content file path inside of zip, destination of package where content file should be extracted)
 		// ex: creteZip("/src/MyFile.txt");
 		// ex: extractZip("MyFile.txt", "/src/destionation");
         // in this case you can relay on file Path system in IDE as long as you are not working on something big.
 	}
 }
+
